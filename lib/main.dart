8d462b805695d1aaf35ce34108058d8f424583dd
Fahim Chowdhury <fahim.arisaftech@gmail.com>
2023-04-09 14:47:03 +0600

    custom scaffolding added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +12,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black
+          )
+      )
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            "Flutter Demo Home Page"
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Amazon Clone"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                "Flutter Demo Home Page"
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('click')
+            )
+          ],
         ),
       ),
     );
