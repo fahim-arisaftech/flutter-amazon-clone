8f2c995673eefb91c6a49104858029e4a006de56
Fahim Chowdhury <fahim.arisaftech@gmail.com>
2023-04-10 00:55:49 +0600

    user can successfully signin from flutter and navigate to homepage

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -44,7 +44,11 @@ class _AuthScreenState extends State<AuthScreen> {
   }
 
   void signInUser() {
-
+    authService.signInUser(
+      context: context,
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
   }
 
   @override
---
+++ lib/features/home/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "/home";
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Welcome Home"),
+    );
+  }
+}
